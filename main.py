82a2a067c0b1ecf702bbaf587151e8645583e2e2
tkvitko <tkvitko@gmail.com>
2021-02-01 18:54:53 +0300

    Ability to set date wanted in config.ini

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ chrome_options.add_argument('start-maximized')
 # на всякий случай, без этого могло упасть по timeout:
 chrome_options.add_argument('enable-features=NetworkServiceInProcess')
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome('../chromedriver', options=chrome_options)
+driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
 TIMES_LIST = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00',
               '14:30', '15:00', '15:30']
